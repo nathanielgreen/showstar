55fddb102ca5ec3f76d22f4983fd23fd4d07f01e
nathanielgreen <ngreen2303@gmail.com>
2016-01-18 17:40:39 +0000

    Completed gallery, planyourparty and music pages

--- app.rb
+++ app.rb
@@ -1,14 +1,29 @@
 # app.rb
 require 'sinatra'
 
+#navbar
 get '/' do
   erb :index
+end
+
+get '/gallery' do
+  erb :gallery
+end
+
+get '/planyourparty' do
+  erb :planyourparty
+end
+
+get '/music' do
+  erb :music
 end
 
 get '/contact' do
   erb :contact
 end
 
+
+#events
 get '/playhouse' do
   erb :playhouse
 end
