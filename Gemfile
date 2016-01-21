7535448c1b0050137e3c99754051bdb3e4607903
nathanielgreen <ngreen2303@gmail.com>
2016-01-21 15:49:04 +0000

    Mail Testing

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 
 gem "ruby"
 gem "sinatra"
+gem "pony"
+
--- app.rb
+++ app.rb
@@ -1,50 +1,62 @@
 # app.rb
 require "bundler/setup"
 require 'sinatra'
-
-#navbar
-get '/' do
-  erb :index
-end
-
-get '/gallery' do
-  erb :gallery
-end
-
-get '/planyourparty' do
-  erb :planyourparty
-end
-
-get '/news' do
-  erb :news
-end
-
-get '/music' do
-  erb :music
-end
-
-get '/contact' do
-  erb :contact
-end
+require 'pony'
 
 
-#events
-get '/playhouse' do
-  erb :playhouse
-end
+  #navbar
+  get '/' do
+    erb :index
+  end
 
-get '/xothursdays' do
-  erb :xothursdays
-end
+  get '/gallery' do
+    erb :gallery
+  end
 
-get '/backwhen' do
-  erb :backwhen
-end
+  get '/planyourparty' do
+    erb :planyourparty
+  end
 
-get '/mayhem' do
-  erb :mayhem
-end
+  get '/news' do
+    erb :news
+  end
 
-get '/selectedsounds' do
-  erb :selectedsounds
-end
+  get '/music' do
+    erb :music
+  end
+
+  get '/contact' do
+    erb :contact
+  end
+
+  post '/contact' do
+    name = params[:name]
+    mail = params[:mail]
+    subject = params[:subject]
+    body = params[:body]
+    Pony.mail(:to => 'ngreen2303@gmail.com', :from => "#{mail}", :subject => "#{subject}", :body => "#{body}")
+    erb :contact
+  end
+
+
+  #events
+  get '/playhouse' do
+    erb :playhouse
+  end
+
+  get '/xothursdays' do
+    erb :xothursdays
+  end
+
+  get '/backwhen' do
+    erb :backwhen
+  end
+
+  get '/mayhem' do
+    erb :mayhem
+  end
+
+  get '/selectedsounds' do
+    erb :selectedsounds
+  end
+
